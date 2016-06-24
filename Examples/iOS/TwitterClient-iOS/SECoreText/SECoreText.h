dfe16f27b8f921f82af1078e4913f480ceecd587
matouka-suzuki <realm.matouka.suzuki@gmail.com>
2016-06-24 15:21:09 +0900

    Add iOS Framework Target

---
+++ Examples/iOS/TwitterClient-iOS/SECoreText/SECoreText.h
@@ -0,0 +1,19 @@
+//
+//  SECoreText.h
+//  SECoreText
+//
+//  Created by 田中 順也 on 2016/06/24.
+//  Copyright © 2016年 kishikawa katsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SECoreText.
+FOUNDATION_EXPORT double SECoreTextVersionNumber;
+
+//! Project version string for SECoreText.
+FOUNDATION_EXPORT const unsigned char SECoreTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SECoreText/PublicHeader.h>
+
+
